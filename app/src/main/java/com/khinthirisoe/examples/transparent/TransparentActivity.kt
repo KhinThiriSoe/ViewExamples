36f65cc7d0eacc3527187f049ddf8fa02d875550
Khin <khinthirisoe.mdy@gmail.com>
2020-02-06 19:00:22 +0700

    transparent activity

---
+++ app/src/main/java/com/khinthirisoe/examples/transparent/TransparentActivity.kt
@@ -0,0 +1,13 @@
+package com.khinthirisoe.examples.transparent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.khinthirisoe.examples.R
+
+class TransparentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transparent)
+    }
+}
