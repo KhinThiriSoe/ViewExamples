35de446b34f4bbefa1c5ddb876309dd68dee672b
Khin <khinthirisoe.mdy@gmail.com>
2020-02-11 10:07:48 +0700

    custom fonts

---
+++ app/src/main/java/com/khinthirisoe/examples/customfonts/CustomFontsActivity.kt
@@ -0,0 +1,13 @@
+package com.khinthirisoe.examples.customfonts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.khinthirisoe.examples.R
+
+class CustomFontsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_fonts)
+    }
+}
